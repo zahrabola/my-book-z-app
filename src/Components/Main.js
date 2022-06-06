5528abf84ec1c595e7039b2265ccba03b84768e6
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-06 09:56:35 +0100

    Starting Book search

---
+++ src/Components/Main.js
@@ -0,0 +1,30 @@
+import react from "react";
+import { FaSearch} from "react-icons/fa";
+const Main = ()=>{
+    return (
+      <>
+        <div className="Header">
+          <div className="row-1">
+            <h1>
+              “Good friends, good books, and a sleepy conscience: <br />
+              this is the ideal life.”
+            </h1>
+          </div>
+          <div className="row-2">
+            <h2>Find a Book</h2>
+            <div className="search">
+              <input type="text" placeholder="Enter Book Name" />
+              <button>
+                <FaSearch />
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+}
+
+
+
+
+export default Main;
